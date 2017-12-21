7a146eaee7b38f799fce2863e0d47140a149338e
alzekiel09 <aixonairashii@gmail.com>
2017-12-21 15:59:02 +0800

    fix bug auto translate pop-up

--- view-schedule.php
+++ view-schedule.php
@@ -10,6 +10,7 @@ require 'lib/Db.config.pdo.php';
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="google" content="notranslate">
     <link rel="shortcut icon" href="img/favicon.ico">
 
     <title>View Schedule</title>
