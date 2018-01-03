9a56f568e561671fe1091f9fd9e93d56c349749f
alzekiel09 <aixonairashii@gmail.com>
2018-01-04 02:18:25 +0800

    adjustment

--- add-patient.php
+++ add-patient.php
@@ -747,20 +747,20 @@ require 'lib/Db.config.php';
 	<script language="JavaScript">
 			Webcam.set({
 				// live preview size
-				width: 320,
-				height: 240,
-				
-				// device capture size
-				dest_width: 640,
-				dest_height: 480,
-				
-				// final cropped size
-				crop_width: 480,
-				crop_height: 480,
-				
-				// format and quality
-				image_format: 'jpeg',
-				jpeg_quality: 90,
+			width: 320,
+			height: 240,
+			
+			// device capture size
+			dest_width: 320,
+			dest_height: 240,
+			
+			// final cropped size
+			crop_width: 240,
+			crop_height: 240,
+			
+			// format and quality
+			image_format: 'jpeg',
+			jpeg_quality: 90,
 				
 				// flip horizontal (mirror mode)
 				flip_horiz: true
