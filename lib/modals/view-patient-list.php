360a52de3258a90c54faf2b578a83ef6b49db1bc
alzekiel09 <aixonairashii@gmail.com>
2018-02-03 15:27:25 +0800

    added patient report

---
+++ lib/modals/view-patient-list.php
@@ -0,0 +1,52 @@
+<!-- Modal Medical Records-->
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="patientlist" class="modal fade">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                          <h4 class="modal-title">Patient List Month of January</h4>
+                      </div>
+                      <div class="modal-body">
+					  <div class="adv-table">
+								<table  class="display table table-bordered table-striped" id="example">
+								  <thead>
+								  <tr>
+									  <th>Patient No.</th>
+									  <th>Full Name</th>
+									  <th>Gender</th>
+									  <th class="hidden-phone">Type</th>
+								  </tr>
+								  </thead>
+								  <tbody>
+								  <tr class="gradeX">
+									  <td>P1</td>
+									  <td>Alessander Neal</td>
+									  <td>Male</td>
+									  <td class="center hidden-phone">Adult</td>
+								  </tr>
+								  <tr class="gradeX">
+									  <td>P1</td>
+									  <td>Alessander Neal</td>
+									  <td>Male</td>
+									  <td class="center hidden-phone">Adult</td>
+								  </tr>
+								  <tr class="gradeX">
+									  <td>P1</td>
+									  <td>Alessander Neal</td>
+									  <td>Male</td>
+									  <td class="center hidden-phone">Adult</td>
+								  </tr>
+								  </tbody>
+								  </table>
+							</div><br><br>
+					</div>
+                      <div class="modal-footer">
+                        <span id="Error_Message" style="float: left; font-weight: bold;" class="text-danger"></span>
+                        <span id="Success_Message" style="float: left; font-weight: bold;" class="text-success"></span>
+          					<a data-dismiss="modal" class="btn btn-shadow btn-default" type="button">Cancel</a>
+          					<a  class="btn btn-shadow btn-success" onclick="addMedicalRecord(<?php echo $Doctor['User_id']?>)"><i class="icon-check">&nbsp;</i>Save</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+<!-- modal medical record end-->
--- patient-chart-report.php
+++ patient-chart-report.php
@@ -11,20 +11,14 @@ require 'lib/session.php';
     <link rel="shortcut icon" href="img/favicon.ico">
 
     <title>Patient Reports Panel</title>
-	<!-- js placed at the end of the document so the pages load faster -->
-    <!--<script src="js/jquery.js"></script>-->
-    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
-    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
-    <script src="js/respond.min.js" ></script>
+
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-reset.css" rel="stylesheet">
     <!--external css-->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/advanced-datatable/media/css/demo_page.css" rel="stylesheet" />
+    <link href="assets/advanced-datatable/media/css/demo_table.css" rel="stylesheet" />
     <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
     <link href="css/style-responsive.css" rel="stylesheet" />
@@ -175,15 +169,6 @@ require 'lib/session.php';
 												<div class="col-lg-2 pull-right">
 													<select id="pyear" class="form-control">
 														<option hidden value="<?php 
-														if(isset($_GET['year'])){
-															$value=$_GET['year']; 
-															echo $value;
-														}
-															else{
-																echo date('Y');
-															}
-																	   ?>">
-															<?php 
 															if(isset($_GET['year'])){
 																$value=$_GET['year']; 
 																echo $value;
@@ -191,7 +176,16 @@ require 'lib/session.php';
 															else{
 																echo date('Y');
 															}
-															?></option>
+														?>">
+														<?php 
+														if(isset($_GET['year'])){
+															$value=$_GET['year']; 
+															echo $value;
+														}
+														else{
+															echo date('Y');
+														}
+														?></option>
 														<?php
 														for($y=2012; $y<=2025; $y++){
 														?>
@@ -229,42 +223,42 @@ require 'lib/session.php';
 												  <tr>
 													  <td class="text-center"><b>January</b></td>
 													  <td class="text-center"><span class="label label-info label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>February</b></td>
 													  <td class="text-center"><span class="label label-primary label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>March</b></td>
 													  <td class="text-center"><span class="label label-success label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>April</b></td>
 													  <td class="text-center"><span class="label label-danger label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>May</b></td>
 													  <td class="text-center"><span class="label label-info label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>June</b></td>
 													  <td class="text-center"><span class="label label-primary label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>July</b></td>
 													  <td class="text-center"><span class="label label-success label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>August</b></td>
 													  <td class="text-center"><span class="label label-danger label-mini">11</span></td>
-													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs" data-toggle="modal" data-target="#patientlist"><i class="icon-eye-open"></i> View</a></td>
 												  </tr>
 												  <tr>
 													  <td class="text-center"><b>September</b></td>
@@ -285,10 +279,15 @@ require 'lib/session.php';
 													  <td class="text-center"><b>December</b></td>
 													  <td class="text-center"><span class="label label-danger label-mini">11</span></td>
 													  <td class="text-center"><a class="btn btn-shadow btn-success btn-xs"><i class="icon-eye-open"></i> View</a></td>
+				
 												  </tr>
 												  </tbody>
 											  </table>
+											 
 										  </div>
+										  <?php
+													include 'lib/modals/view-patient-list.php';
+												?>
 									  </div>
 								  </div>
 							  </section>
@@ -316,139 +315,26 @@ require 'lib/session.php';
 <?php
 include 'lib/User-Accesslvl.php';
 ?>
-	<script>
-		$('.btn').on('click', function() {
-			var $this = $(this);
-		  $this.button('loading');
-			setTimeout(function() {
-			   $this.button('reset');
-		   }, 8000);
-		});
-	</script>
+	<!-- js placed at the end of the document so the pages load faster -->
+    <!--<script src="js/jquery.js"></script>-->
+    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
+    <script src="js/respond.min.js" ></script>
+	<script type="text/javascript" charset="utf-8">
+          $(document).ready(function() {
+              $('#example').dataTable( {
+                  "aaSorting": [[ 4, "desc" ]]
+              } );
+          } );
+      </script>
   <!--common script for all pages-->
     <script src="js/common-scripts.js"></script>
     <script src = "js/jquery.canvasjs.min.js"></script>
-	
-<script type="text/javascript"> 
-	window.onload = function(){ 
-		$("#patient_population").CanvasJSChart({
-			theme: "light2",
-			zoomEnabled: true,
-			zoomType: "x",
-			panEnabled: true,
-			animationEnabled: true,
-			animationDuration: 1000,
-			exportFileName: "Monthly Population", 
-			exportEnabled: true,
-			toolTip: {
-                shared: true  
-            },
-			title: { 
-				text: "Patient Population as of Year ",
-				fontSize: 20
-			},
-			legend: {
-				cursor: "pointer",
-				itemclick: function (e) {
-					if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-						e.dataSeries.visible = false;
-					} else {
-						e.dataSeries.visible = true;
-					}
-					e.chart.render();
-				}
-			},
-			axisX: {		       
-				gridDashType: "dot",
-				gridThickness: 1,
-				labelFontColor: "black",
-				crosshair: {
-					enabled: true 
-				}
-			},
-			axisY: { 
-				title: "Total Population", 
-				includeZero: false,
-				labelFontColor: "black",
-				crosshair: {
-					enabled: true 
-				}
-			}, 
-			data: [ 
-				{ 
-					type: "column", 
-					showInLegend: true, 
-					legendText: "Total",
-					name: "Total", 
-					toolTipContent: "{label}: {y}", 
-					dataPoints: [ 
-						{ label: "January", y: 200 },
-						 { label: "February", y: 100 },
-						{ label: "March", y: 20 },
-						 { label: "April", y: 50 },
-						{ label: "May", y: 100 },
-						 { label: "June", y: 300 },
-						{ label: "July", y: 250 },
-						 { label: "August", y: 159 },
-						{ label: "September", y: 20 },
-						 { label: "October", y: 70 },
-						{ label: "November", y: 90 },
-						 { label: "December", y: 120 }
-					] 
-				},
-
-
-				{ 
-					type: "spline", 
-					showInLegend: true, 
-					legendText: "Children",
-					name: "Children", 
-					dataPoints: [ 
-						{ label: "January", y: 120 },
-						 { label: "February", y: 60 },
-						{ label: "March", y: 20 },
-						 { label: "April", y: 50 },
-						{ label: "May", y: 104 },
-						 { label: "June", y: 310 },
-						{ label: "July", y: 280 },
-						 { label: "August", y: 259 },
-						{ label: "September", y: 200 },
-						 { label: "October", y: 73 },
-						{ label: "November", y: 91 },
-						 { label: "December", y: 10 }
-					] 
-				},
-				{ 
-					type: "spline", 
-					showInLegend: true, 
-					legendText: "Adult",
-					name: "Adult", 
-					dataPoints: [ 
-						{ label: "January", y: 100 },
-						 { label: "February", y: 160 },
-						{ label: "March", y: 205 },
-						 { label: "April", y: 80 },
-						{ label: "May", y: 14 },
-						 { label: "June", y: 31 },
-						{ label: "July", y: 28 },
-						 { label: "August", y: 59 },
-						{ label: "September", y: 100 },
-						 { label: "October", y: 13 },
-						{ label: "November", y: 31 },
-						 { label: "December", y: 90 }
-					] 
-				}
-			] 
-		}); 
-	}
-</script>
-	<script>
-            $(document).ready(function(){
-                $("#pyear").on('change', function(){
-                    var year=$(this).val();
-                    window.location = 'patient-chart-report.php?year='+year;
-                });
-            });
-    </script>
+	<?php require 'reports/charts/patient_population.php'?>
+		
   </body>
 </html>
