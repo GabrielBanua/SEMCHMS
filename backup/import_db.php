42f5c3b347f1acea3fefb78afc0b159bc5e525cc
yokam03 <yokam03@gmail.com>
2018-02-09 23:44:10 +0800

    als e modify lihog tyty

--- backup/import_db.php
+++ backup/import_db.php
@@ -1,18 +1,53 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', '', 'semhcms');
+set_time_limit(0);
+$message = '';
+$message2 = '';
+if(isset($POST_["import"])){
+        if($_FILES["database"]["name"] != '')
+        {
+            $array = explode(".", $_FILES["database"]["name"]);
+            $extension == end($Array);
+            if($extension == 'sql')
+            {
+                $connect = mysqli_connect("localhost", "root","". "semhcms");
+                $output = '';
+                $count = 0;
+                $file_data - file($_FILES["database"]["tmp_name"]);
+                 
+                foreach($file_data as $row)
+                {
+                    $start_Character = substr(trim($row), 0,2);
+                    if($start_Character != '--' || $start+Character != '/*' || $start_Character != '//' || $row !='')
+                    {
+                        $output = $outout . $row;
+                        $end_character = substr(trim($row), -1,1);
+                        if($end_character = ';')
+                        {
+                            if(!mysqli_query($connect,$outout))
+                            {
+                                $count++;
+                            }
+                            $output = '';
+                            }
+                        }
+                    }
+                    $messege = '<label class = "text-success"> Database successfully imported!</label>';
+                } else
+                 {
+                    $message = '<label class = "text-danger"> Invalid file</label>';
+                }
+            } else 
+            {
+                $message = '<label class = "Text-danger"> Please select SQL file</label>';
+            }
+            date_default_timezone_get('Asia/Mani la');
+            $date = date ('d-M-y');
+            $time = time ('h:i:s');
 
-$filename = 'semhcms.sql';
-$handle = fopen($filename, "r+");
-$contents = fread($handle, filesize($filename));
+            $con->query("INSERT INTO `backuphistory` VALUES ('','Export', '$date', '$time')") or die(mysqli_error());
+        }
 
-$sql = explode(';', $contents);
-foreach ($sql as $query){
-    $result = mysqli_query($connection, $query);
-
-}
-fclose($handle);
-echo "<script>alert('Successfully imported database!')</script>";
-echo "<script>document.location='../backup.php'</script>";  
 
 
 ?>
