b0d4847098d32026047d53bf81be2b6003a78346
alzekiel09 <aixonairashii@gmail.com>
2018-03-10 00:07:19 +0800

    test inventory

---
+++ inventory.php
@@ -0,0 +1,293 @@
+<?php
+require 'lib/session.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <title>Backup Database</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/advanced-datatable/media/css/demo_page.css" rel="stylesheet" />
+    <link href="assets/advanced-datatable/media/css/demo_table.css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap-fileupload/bootstrap-fileupload.css" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 tooltipss and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+  <section id="container" class="">
+      <!--header start-->
+      <header class="header white-bg">
+          <div class="sidebar-toggle-box">
+              <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+          </div>
+          <!--logo start-->
+          <a href="index.html" class="logo" >St. Ezekiel Moreno<span>|Healthcare Management System</span></a>
+          <!--logo end-->
+          <div class="top-nav ">
+              <ul class="nav pull-right top-menu">
+                  <li>
+                      <input type="text" class="form-control search" placeholder="Search">
+                  </li>
+                  <!-- user login dropdown start-->
+                  <li class="dropdown">
+                      <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                          <img alt="" src="img/avatar1_small.jpg">
+                          <span class="username"><b><?php echo $UserN; ?></b></span>
+                          <b class="caret"></b>
+                      </a>
+                      <ul class="dropdown-menu extended logout">
+                          <div class="log-arrow-up"></div>
+                          <li><a href="#"><i class=" icon-suitcase"></i>Profile</a></li>
+                          <li><a href="#"><i class="icon-cog"></i> Settings</a></li>
+                          <li><a href="#"><i class="icon-bell-alt"></i> Notification</a></li>
+                          <li><a onclick="logout()"><i class="icon-key"></i> Log Out</a></li>
+                      </ul>
+                  </li>
+                  <!-- user login dropdown end -->
+              </ul>
+          </div>
+      </header>
+      <!--header end-->
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                  <li>
+                      <a href="index.php">
+                          <i class="icon-dashboard"></i>
+                          <span>Home</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu" id="Patient-li">
+                      <a href="javascript:;" >
+                          <i class="icon-user"></i>
+                          <span>Patient Management</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="add-patient.php">Add Patients</a></li>
+                          <li><a  href="view-patients.php">View Patients</a></li>
+						  <li><a  href="patient-reports-panel.php">Patient Reports</a></li>
+                      </ul>
+                  </li>
+				  
+				  <li class="sub-menu" id="Schedule-li">
+                      <a href="javascript:;">
+                          <i class="icon-calendar"></i>
+                          <span>Schedule Management</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="set-schedule.php">Set Schedule</a></li>
+                          <li><a  href="view-schedule.php">View Schedule</a></li>
+						  <li><a  href="sched-reports-panel.php">Schedule Reports</a></li>
+                      </ul>
+                  </li>
+				  
+				  <li class="sub-menu" id="Inventory-li">
+                      <a href="javascript:;" >
+                          <i class="icon-truck"></i>
+                          <span>Inventory Management</span>
+                      </a>
+                      <ul class="sub">
+						  <li><a href="view-inventory.php">View Inventory</a></li>
+						  <li><a href="inv-reports-panel.php">Inventory Reports</a></li>
+                      </ul>
+                  </li>
+				  
+				  <li class="sub-menu" id="Laboratory-li">
+                      <a href="javascript:;">
+                          <i class="icon-beaker"></i>
+                          <span>Lab Management</span>
+                      </a>
+                      <ul class="sub">
+						  <li><a  href="lab-request.php">View Lab Request</a></li>
+						  <li><a  href="labview.php">View Lab Records</a></li>
+						  <li><a  href="lab-reports-panel.php">Laboratory Reports</a></li>
+                      </ul>
+                  </li>
+				  
+				  <li class="sub-menu" id="Maintenance-li">
+                      <a href="javascript:;" class="active">
+                          <i class="icon-download-alt"></i>
+                          <span>Maintenance</span>
+                      </a>
+                      <ul class="sub">
+                          <li class="active"><a  href="backup.php">Backup Database</a></li>
+						  <li><a  href="view-users.php">Manage Users</a></li>
+						  <li><a  href="systemlogs.php">System Logs</a></li>
+                      </ul>
+                  </li>
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">
+              <!-- page start-->
+              <div class="row">
+                  <div class="col-lg-12">
+                      <section class="panel">
+                          <header class="panel-heading">
+                              Users
+                          </header>
+                          <div class="panel-body">
+                                <div class="adv-table">
+									<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="hidden-table-info">
+                                <thead>
+                                <tr>
+                                    <th>Rendering engine</th>
+                                    <th>Browser</th>
+                                    <th class="hidden-phone">Platform(s)</th>
+                                    <th class="hidden-phone">Engine version</th>
+                                    <th class="hidden-phone">CSS grade</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="gradeX">
+                                    <td>Trident</td>
+                                    <td>Internet
+                                        Explorer 4.0</td>
+                                    <td class="hidden-phone">Win 95+</td>
+                                    <td class="center hidden-phone">4</td>
+                                    <td class="center hidden-phone">X</td>
+                                </tr>
+                                <tr class="gradeC">
+                                    <td>Trident</td>
+                                    <td>Internet
+                                        Explorer 5.0</td>
+                                    <td class="hidden-phone">Win 95+</td>
+                                    <td class="center hidden-phone">5</td>
+                                    <td class="center hidden-phone">C</td>
+                                </tr>
+								<tr class="gradeU">
+                                    <td>Other browsers</td>
+                                    <td>All others</td>
+                                    <td class="hidden-phone">-</td>
+                                    <td class="center hidden-phone">-</td>
+                                    <td class="center hidden-phone">U</td>
+                                </tr>
+                                </tbody>
+									</table>
+                                </div>
+                          </div>
+                      </section>
+                  </div>
+              </div>
+              <!-- page end-->
+          </section>
+      </section>
+      <!--main content end-->
+      <!--footer start-->
+      <footer class="site-footer">
+          <div class="text-center">
+              2017-2018 &copy; Primal Tinkers.
+              <a href="#" class="go-top">
+                  <i class="icon-angle-up"></i>
+              </a>
+          </div>
+      </footer>
+      <!--footer end-->
+  </section>
+
+        <!-- js placed at the end of the document so the pages load faster -->
+    <!--<script src="js/jquery.js"></script>-->
+    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="js/respond.min.js" ></script>
+    <script type="text/javascript" language="javascript" src="assets/advanced-datatable/media/js/jquery.dataTables.js"></script>
+
+
+    <!--common script for all pages-->
+    <script src="js/common-scripts.js"></script>
+
+    <script type="text/javascript">
+      /* Formating function for row details */
+      function fnFormatDetails ( oTable, nTr )
+      {
+          var aData = oTable.fnGetData( nTr );
+          var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+          sOut += '<tr><td>Rendering engine:</td><td>'+aData[1]+' '+aData[4]+'</td></tr>';
+          sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
+          sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+          sOut += '</table>';
+
+          return sOut;
+      }
+
+      $(document).ready(function() {
+          /*
+           * Insert a 'details' column to the table
+           */
+          var nCloneTh = document.createElement( 'th' );
+          var nCloneTd = document.createElement( 'td' );
+          nCloneTd.innerHTML = '<img src="assets/advanced-datatable/examples/examples_support/details_open.png">';
+          nCloneTd.className = "center";
+
+          $('#hidden-table-info thead tr').each( function () {
+              this.insertBefore( nCloneTh, this.childNodes[0] );
+          } );
+
+          $('#hidden-table-info tbody tr').each( function () {
+              this.insertBefore(  nCloneTd.cloneNode( true ), this.childNodes[0] );
+          } );
+
+          /*
+           * Initialse DataTables, with no sorting on the 'details' column
+           */
+          var oTable = $('#hidden-table-info').dataTable( {
+              "aoColumnDefs": [
+                  { "bSortable": false, "aTargets": [ 0 ] }
+              ],
+              "aaSorting": [[1, 'asc']]
+          });
+
+          /* Add event listener for opening and closing details
+           * Note that the indicator for showing which row is open is not controlled by DataTables,
+           * rather it is done here
+           */
+          $('#hidden-table-info tbody td img').live('click', function () {
+              var nTr = $(this).parents('tr')[0];
+              if ( oTable.fnIsOpen(nTr) )
+              {
+                  /* This row is already open - close it */
+                  this.src = "assets/advanced-datatable/examples/examples_support/details_open.png";
+                  oTable.fnClose( nTr );
+              }
+              else
+              {
+                  /* Open this row */
+                  this.src = "assets/advanced-datatable/examples/examples_support/details_close.png";
+                  oTable.fnOpen( nTr, fnFormatDetails(oTable, nTr), 'details' );
+              }
+          } );
+      } );
+  </script>
+<?php
+include 'lib/User-Accesslvl.php';
+include 'lib/logout.script.php';
+?>
+  </body>
+</html>
